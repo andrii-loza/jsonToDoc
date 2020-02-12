d41be8e63cd86b6efee8e5be418a7189e917a55c
Andrii Loza <loza.andriy7@gmail.com>
2020-02-12 16:50:02 +0200

    compress after todo some features

--- jsontodoc/public/javascripts/compress.js
+++ jsontodoc/public/javascripts/compress.js
@@ -1,7 +1,6 @@
 let ext = null;
 let quality = null;
 let idSelect = null;
-let idErrorMsg = null;
 
 
 function findExt(type) {
@@ -9,16 +8,28 @@ function findExt(type) {
     else if (type === 'image/jpeg') ext = 'jpg';
 }
 
+function calculateSizeToBytes(totalBytes) {
+    let _size;
+    if (totalBytes < 1000000) {
+        _size = (totalBytes / 1024).toFixed(2) + ' KB';
+        return _size
+    } else {
+        _size = (totalBytes / 1024000).toFixed(2) + ' MB';
+        return _size
+    }
+}
+
 function readURL(input, callback) {
     quality = null;
     if (input.files && input.files[0]) {
+        const size = calculateSizeToBytes(input.files[0].size);
+        document.getElementById('size_not_compressed').innerText = `Size: ${size}`;
         findExt(input.files[0].type);
         let reader = new FileReader();
 
         reader.onload = function (e) {
             document.getElementById('img').setAttribute('src', e.target.result);
-            document.getElementById('btn').disabled = false;
-            if (idSelect && isSelectOrTextField(idSelect)) {
+            if (idSelect && isSelect(idSelect)) {
                 document.getElementById(idSelect).remove();
             }
             callback()
@@ -45,6 +56,7 @@ function createSelect() {
     select.appendChild(opt);
     select.addEventListener('change', function () {
         quality = this.options[this.selectedIndex].value;
+        document.getElementById('btn').disabled = false;
     });
 
     wrapper.appendChild(select);
@@ -65,39 +77,34 @@ function createSelect() {
     }
 }
 
-function isSelectOrTextField(idName) {
+function isSelect(idName) {
     const element = document.getElementById(idName);
-    if (element.tagName === 'SELECT') {
-        return true;
-    }
-    if (element.tagName === 'P') {
-        return true;
-    }
+    if (element.tagName === 'SELECT') return true;
     return false;
 }
 
 function handleData() {
     if (quality) {
-        if (idErrorMsg && isSelectOrTextField(idErrorMsg)) {
-            document.getElementById(idErrorMsg).remove();
-        }
-        idErrorMsg=null;
         const src_image = document.getElementById('img').getAttribute('src');
-        fetch('http://localhost:3000/image-compress/compress', {
+        fetch('/image-compress/compress', {
             method: 'post',
             body: JSON.stringify({src: src_image, quality, ext}),
             headers: {'Content-type': 'application/json'}
         }).then(res => res.text())
             .then(res => {
                 document.getElementById('compressed_img').setAttribute('src', res);
+                checkSize(res);
             })
-    } else {
-        const p = document.createElement('p');
-        p.style.color = 'red';
-        p.setAttribute('id', 'errorMsg');
-        idErrorMsg = p.getAttribute('id');
-        const div = document.getElementById('wrapper_select');
-        p.innerText = `Quality didn't select`;
-        div.appendChild(p);
     }
+}
+
+function checkSize(imageUrl) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', imageUrl, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function () {
+        const size = calculateSizeToBytes(xhr.response.size);
+        document.getElementById('size_compressed').innerText = `Size: ${size}`;
+    };
+    xhr.send();
 }

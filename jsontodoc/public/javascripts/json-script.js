e2cc7c1f3d765470e27588e476f63169c34b6604
Andrii Loza <loza.andriy7@gmail.com>
2020-02-13 11:40:11 +0200

    add comments

--- jsontodoc/public/javascripts/json-script.js
+++ jsontodoc/public/javascripts/json-script.js
@@ -21,6 +21,8 @@ $(function () {
                 let res = JSON.parse(xhr.response);
                 if(res) alert('Now check ./files folder in the project root directory');
 
+                //to download file from back-end
+
                 // let content = xhr.response;
                 // let fileName = 'rapport.docx';
                 // downloadwithpost(fileName, content);
@@ -42,22 +44,22 @@ $(function () {
         if (e.target.files.length > 0) document.querySelector('#send').style.display = 'inline-block';
     });
 
-    function downloadwithpost(filename, content) {
-        let link = document.createElement('a');
-        let bytes = new Array(content.length);
-        for (let i = 0; i < content.length; i++) {
-            bytes[i] = content.charCodeAt(i);
-        }
-        let byteArray = new Uint8Array(bytes);
-        let blob = new Blob([byteArray], {
-            type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        });
-        let url = URL.createObjectURL(blob);
-
-        let a = document.createElement('a');
-        a.href = url;
-        a.download = filename;
-        a.click();
-        window.URL.revokeObjectURL(url);
-    }
+    // function downloadwithpost(filename, content) {
+    //     let link = document.createElement('a');
+    //     let bytes = new Array(content.length);
+    //     for (let i = 0; i < content.length; i++) {
+    //         bytes[i] = content.charCodeAt(i);
+    //     }
+    //     let byteArray = new Uint8Array(bytes);
+    //     let blob = new Blob([byteArray], {
+    //         type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    //     });
+    //     let url = URL.createObjectURL(blob);
+    //
+    //     let a = document.createElement('a');
+    //     a.href = url;
+    //     a.download = filename;
+    //     a.click();
+    //     window.URL.revokeObjectURL(url);
+    // }
 });
